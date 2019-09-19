c0549ba0d563e3cfd35a9691ddab3f55fb199b4d
Perry Brandiezs <pbrandiezs@gmail.com>
2019-09-19 15:13:19 -0700

    feat: Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name udaciMealsApp
+ * @description
+ * # udaciMealsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('udaciMealsApp', [
+    'ngAnimate',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
